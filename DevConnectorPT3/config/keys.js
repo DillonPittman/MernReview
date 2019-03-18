8dfb772a74034f1917e812d3b01edfc62c21a333
DillonPittman <pittmanwdillon@gmail.com>
2019-03-17 23:53:22 -0500

    initial commit

---
+++ DevConnectorPT3/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI:
+		"mongodb://pittmanwdillon:ch1k1n@ds231725.mlab.com:31725/devconnector",
+	secretOrKey: "secret",
+};
---
+++ devconnector-S4/devconnector_S4/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI:
+		"mongodb://pittmanwdillon:ch1k1n@ds123822.mlab.com:23822/heroku_7l937ph4",
+	secretOrKey: "secret",
+};
